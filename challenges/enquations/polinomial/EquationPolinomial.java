a73df6e74d72453fafef1a1969f3c4763a5b82a3
jedin01 <abnerjaredejede@gmail.com>
2025-03-03 11:46:57 +0100

    mar03 11:46

---
+++ challenges/enquations/polinomial/EquationPolinomial.java
@@ -0,0 +1,16 @@
+package enquations.polinomial;
+
+import java.util.List;
+
+public class EquationPolinomial {
+    private String leiFormacao;
+    private Double coeficienteAngular;
+    private Double coeficienteLinear;
+    private List<Integer> raizes;
+    private List<String> monotonia;
+    private int grau;
+
+    public EquationPolinomial(int grau, Double coeficienteAngular, Double coeficienteLinear){
+
+    }
+}
--- challenges/enquations/polinomial/LinearEquation.java
+++ challenges/enquations/polinomial/LinearEquation.java
@@ -1,4 +1,20 @@
 package enquations.polinomial;
 
+import java.util.List;
+
 public class LinearEquation {
+
+    private String leiFormacao;
+    private Double coeficienteAngular;
+    private Double coeficienteLinear;
+    private List<Integer> raizes;
+    private int grau;
+    public LinearEquation(Double coeficienteLinear, Double coeficienteAngular){
+        this.grau = 1;
+        this.leiFormacao = coeficienteAngular+""+grau+"x+"+coeficienteLinear;
+    }
+
+    public String getLeiFormacao(){
+        return leiFormacao;
+    }
 }
