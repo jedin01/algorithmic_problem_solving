9ad8e655c17873eac2b8289051669fe22605d027
jedin01 <abnerjaredejede@gmail.com>
2025-03-05 10:15:51 +0100

    mar05 10:15

--- challenges/App.java
+++ challenges/App.java
@@ -1,7 +1,6 @@
 import enquations.polinomial.IdentificadorDeEquacoesPolinomiais;
 import enquations.polinomial.LinearEquation;
 import enquations.polinomial.QuadraticEquation;
-
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -9,7 +8,7 @@ import java.util.regex.Pattern;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println(new LinearEquation("2x-4").getRaizes());
+        System.out.println(new LinearEquation("2x-4").getEstudoCompleto());
 
     }
 }
--- challenges/enquations/polinomial/LinearEquation.java
+++ challenges/enquations/polinomial/LinearEquation.java
@@ -1,6 +1,7 @@
 package enquations.polinomial;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -9,26 +10,27 @@ public class LinearEquation {
     private String leiFormacao;
     private Double coeficienteAngular;
     private Double coeficienteLinear;
-    private List<Double> raizes;
+    private List<Object> raizes;
     private int grau;
     private String monotonia;
-
+    private HashMap<String, Object> estudoCompleto;
     public LinearEquation(String expressao){
 
         this.grau = 1;
         this.raizes = new ArrayList<>();
+        this.estudoCompleto = new HashMap<String, Object>();
         this.coeficienteAngular=Double.parseDouble(expressao.substring(0, expressao.indexOf("x")));
         this.coeficienteLinear=Double.parseDouble(expressao.substring(expressao.indexOf("x")+1, expressao.length()));
         this.monotonia = coeficienteAngular>0?"funcao crescente":"funcao decrescente";
+        this.calcularRaizes();
         this.leiFormacao = coeficienteAngular+(coeficienteLinear<0?"x":"x+")+coeficienteLinear;
     }
-
 
     public String getLeiFormacao(){
         return leiFormacao;
     }
 
-    public List<Double> getRaizes(){
+    public List<Object> getRaizes(){
         calcularRaizes();
         return this.raizes;
     }
@@ -60,8 +62,11 @@ public class LinearEquation {
         }
     }
 
-    public String getDescricao(){
-        return "Esta é uma funcao polinomial linear";
+    public HashMap<String, Object> getEstudoCompleto(){
+        estudoCompleto.put("raizes", raizes);
+        estudoCompleto.put("dominio", "R");
+        estudoCompleto.put("coeficientes", coeficientes)
+        return estudoCompleto;
     }
 
 
