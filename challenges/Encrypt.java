22cf1be38b20d69641c0b27d6225a6e8f3ed1750
jedin01 <abnerjaredejede@gmail.com>
2025-03-07 12:30:26 +0100

    mar07 12:30

--- challenges/Encrypt.java
+++ challenges/Encrypt.java
@@ -1,16 +1,25 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 public class Encrypt {
+
+    private final char[] SYMBOLS;
+
+    {
+        SYMBOLS = new char[94];
+    }
 
     public static void main(String[] args) {
 
     }
 
     public String [] toEncrypt(String[] inputs){
-        StringBuilder[] ins = {
-                for(String in : inputs){
-                    new StringBuilder(in);
-                }
-        }
+        HashMap<Integer, StringBuilder> ins = new HashMap<>();
+       for(int i=32; i<=126; i++){
+           ins.put(i, )
+
+       }
     }
 }
