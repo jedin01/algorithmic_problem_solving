cf51804cbebf2a41e464fc0334a044a1c91ea272
jedin01 <abnerjaredejede@gmail.com>
2025-03-07 12:55:41 +0100

    mar07 12:55

--- challenges/Encrypt.java
+++ challenges/Encrypt.java
@@ -6,9 +6,11 @@ import java.util.List;
 public class Encrypt {
 
     private final char[] SYMBOLS;
+    private List<>
 
     {
         SYMBOLS = new char[94];
+
     }
 
     public static void main(String[] args) {
@@ -16,10 +18,21 @@ public class Encrypt {
     }
 
     public String [] toEncrypt(String[] inputs){
-        HashMap<Integer, StringBuilder> ins = new HashMap<>();
-       for(int i=32; i<=126; i++){
-           ins.put(i, )
 
-       }
+        // first step
+        for (int i= 32; i<=126; i++){
+            int j = 0;
+            inputs[j].toCharArray();
+            j++;
+        }
+    }
+    public void checkPlace(String[] inputs){
+
+        for(String input : inputs){
+
+
+
+        }
+
     }
 }
