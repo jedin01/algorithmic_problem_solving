6a14f7d40976cec889e343eff1a0110455a42b13
jedin01 <abnerjaredejede@gmail.com>
2025-03-08 16:33:23 +0100

    from sofa commit

--- challenges/Encrypt.java
+++ challenges/Encrypt.java
@@ -5,29 +5,16 @@ import java.util.List;
 
 public class Encrypt {
 
-    private final char[] SYMBOLS;
-    private List<>
-
-    {
-        SYMBOLS = new char[94];
-
-    }
-
     public static void main(String[] args) {
-
+        String[] a = {"abner"};
+        String[] k = toEncrypt(a);
+        System.out.println(k);
     }
 
-    public String [] toEncrypt(String[] inputs){
+    public static String [] toEncrypt(String[] inputs){
 
         // first step
-        for(int i = 0; i<inputs.length; i++){
-            for (int j = 32; j<= 126; j++){
-                if(inputs[i].toCharArray()[j-32] == (char) j){
-                    inputs[i].toCharArray()[j-32] = (char) (j+3);
-                }
-            }
-        }
-        return inputs;
+
     }
     public void checkPlace(String[] inputs){
 
